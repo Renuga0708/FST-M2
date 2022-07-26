d556e7128ea17382269c5919943414686365cd0d
Renuga0708 <109344673+Renuga0708@users.noreply.github.com>
2022-07-26 22:41:57 +0530

    Add files via upload

---
+++ junit/Activity2_BankAccount_JUnit.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+public class BankAccount {
+    private Integer balance;
+
+    // Create a constructor
+    public BankAccount(Integer initialBalance) {
+        balance = initialBalance;
+    }
+
+    // Add method to calculate
+    // balance amount after withdrawal
+    public Integer withdraw(Integer amount) {
+        if (balance < amount) {
+            throw new NotEnoughFundsException(amount, balance);
+        }
+        balance -= amount;
+        return balance;
+    }
+}
+
+class ExpectedExceptionTest {
+    @Test
+    void notEnoughFunds() {
+        // Create an object for BankAccount class
+        BankAccount account = new BankAccount(9);
+
+        // Assertion for exception
+        assertThrows(NotEnoughFundsException.class, () -> account.withdraw(10));
+    }
+
+    @Test
+    void enoughFunds() {
+        // Create an object for BankAccount class
+        BankAccount account = new BankAccount(100);
+
+        // Assertion for no exceptions
+        assertDoesNotThrow(() -> account.withdraw(100));
+    }
+}
